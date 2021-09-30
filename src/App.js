f771a8e4e83dc19ad802693c171d707d460aff1f
sumaiya722 <2021381@iub.edu.bd>
2021-10-01 01:54:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
+import { ButtonGroup, Button, Row, Col, Card, Spinner } from 'react-bootstrap';
 import './App.css';
+import News from './Components/News/News';
+// import Card from './Components/Card/Card';
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-30&sortBy=publishedAt&apiKey=1bd15ab3e5024593a26b3b846ef6346d')
+      .then(res => res.json())
+      .then(data => setNews(data.articles));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        news.length === 0 ?
+          <Spinner animation="border" />
+          :
+          <Row xs={1} md={3} className="g-4">
+            {
+              news.map(nw => <News news={nw}></News>)
+            }
+          </Row>
+      }
     </div>
+
   );
 }
 
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Card = (props) => {
+    const { name, img, description } = props.item;
+    return (
+        <div>
+            <div className="col">
+                <div className="card">
+                    <img src={img} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
